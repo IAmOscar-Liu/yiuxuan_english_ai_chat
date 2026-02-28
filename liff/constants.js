539d7f9187a1ce5297e7e699a4c331d3d6803204
Oscar Liu <oscar@symptomtrace.com>
2026-02-28 23:49:15 +0800

    save current status@Feb. 28 23:49

--- liff/constants.js
+++ liff/constants.js
@@ -1,10 +1,10 @@
 // Local
-const LIFF_ID = "2007679854-kyk8M7kj";
-const OA_ID = "@733wkctx";
+// const LIFF_ID = "2007679854-kyk8M7kj";
+// const OA_ID = "@733wkctx";
 
 // production
-// const LIFF_ID = "2007679854-jyoyy0Wq";
-// const OA_ID = "@351sdgdu";
+const LIFF_ID = "2007679854-jyoyy0Wq";
+const OA_ID = "@351sdgdu";
 
 export { LIFF_ID, OA_ID };
 
